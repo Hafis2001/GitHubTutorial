f17626dd190848be2380efe869b4d9597beef223
Hafis <johndoe@example.com>
2024-03-26 12:25:27 +0530

    App.js Updated! to Test

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello it is react Git hub tutorialgit </Text>
+      <Text>Test 1234 </Text>
       <StatusBar style="auto" />
     </View>
   );
